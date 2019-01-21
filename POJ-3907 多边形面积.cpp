7bc4436d5fbf7ca581ee2699a6dc8aff4b76d73a
sweet_fish <34713201+sweet-fish@users.noreply.github.com>
2019-01-21 22:57:45 +0800

    Update and rename POJ-3907 多边形面积 to POJ-3907 多边形面积.cpp

--- POJ-3907 多边形面积.cpp
+++ POJ-3907 多边形面积.cpp
@@ -1,10 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 const double eps=1e-10;
 struct point
 {
     double x,y;
 }p[100005];
+bool cmp(const point &a,const point &b)
+{
+    return (a.x-p[0].x)*(b.y-p[0].y)-(a.y-p[0].y)*(b.x-p[0].x)>=eps;
+}
 double cross(point a,point b)
 {
     return a.x*b.y-a.y*b.x;
@@ -14,17 +19,12 @@ int main()
     int n;
     while(cin>>n&&n)
     {
-        if(n==1)
-        {
-            cout<<0<<endl;
-            continue;
-        }
         for(int i=0;i<n;++i)
             cin>>p[i].x>>p[i].y;
         double ans=0;
         for(int i=0;i<n;++i)
             ans+=cross(p[i],p[(i+1)%n]);
-        ans=round(0.5*fabs(ans));
-        cout<<(int)ans<<endl;
+        ans=0.5*fabs(ans);
+        cout<<int(ans+0.5)<<endl;
     }
 }
